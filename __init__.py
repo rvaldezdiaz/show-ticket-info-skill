eb26ac126f8f8ccb0ca6b37bf7c139de7d2c3a11
Rachel Valdez <rvaldezdiaz@sandiego.edu>
2021-04-21 22:54:05 -0700

    changed the speaker messages for user friendliness

--- __init__.py
+++ __init__.py
@@ -20,14 +20,14 @@ class ShowTicketInfo(MycroftSkill):
         answer = "yes"
         while (answer == "yes"):
             i = 1
-            self.speak('Here are the available tickets.')
+            self.speak('Here are the available tickets: ')
             for row in rows:
                 cur.execute("SELECT * FROM TransitLine WHERE LineID = ?", (row[3],))
                 idrow = cur.fetchone()
-                self.speak('Ticket {} starts at {}, ends at {}, has an E.T.A of {}, and costs ${}.'.format(i, row[4], row[5], idrow[3], row[6]))
+                self.speak('Ticket number {} : starts at {}, ends at {}, has an E.T.A of {}, and costs ${}.'.format(i, row[4], row[5], idrow[3], row[6]))
                 i += 1
 
-            n = int(self.get_response('Which ticket would you like to select?'))
+            n = int(self.get_response('These are all the tickets available. Which ticket would you like to select?'))
             m = n
             n = n - 1
 
@@ -40,12 +40,12 @@ class ShowTicketInfo(MycroftSkill):
 
             self.speak('You have chosen to view following ticket: \n')
             self.speak(' Ticket: {}. Start: {},  End: {},  ETA: {},  Cost: ${}.'.format(m, ticket[4], ticket[5], idrow[3], ticket[6]))
-            #ask what to do after?
-            answer = self.ask_yesno('Would you like to choose another ticket to view? (yes/no): ')
+            #ask user what to do after ticket info is displayed
+            answer = self.ask_yesno('Would you like to choose another ticket to view? Please say yes or no: ')
             if answer == "yes":
                 self.speak('Fetching ticket information...')
 
-        self.speak('Closing the ticket viewer...')
+        self.speak('Thank you. Closing the ticket viewer...')
         conn.close()
         #self.speak_dialog('info.ticket.show')
 
